fa7fbfe49e40baaf32a8d9d2ccd578171bf33b18
MinsungKang <minsungkang2012@gmail.com>
2025-05-02 22:39:34 +0900

    1. Added createdAt with LocalDateTime, 2. Added protected no args constructor so (only) JPA could instantiate entity class, 3. Added @ToString.include to debug and avoid circular reference, 4. Added CommandLineRunner for testing

--- src/main/java/com/min/InsultBoard/InsultBoardApplication.java
+++ src/main/java/com/min/InsultBoard/InsultBoardApplication.java
@@ -1,7 +1,11 @@
 package com.min.InsultBoard;
 
+import com.min.InsultBoard.entity.Insult;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InsultBoardApplication {
@@ -10,4 +14,12 @@ public class InsultBoardApplication {
 		SpringApplication.run(InsultBoardApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			// Create a new Insult object and print it
+			Insult insult = new Insult();
+			System.out.println(insult);
+		};
+	}
 }
--- src/main/java/com/min/InsultBoard/entity/Insult.java
+++ src/main/java/com/min/InsultBoard/entity/Insult.java
@@ -4,10 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Entity
+@ToString
 public class Insult {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @ToString.Include
     private Long id;
+
+    @ToString.Include
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Insult() {}
 }
